739c7b033596cd9f37b57f750cc3743f6769cc9a
nja-n <v4vishnumg25@gmail.com>
2026-01-03 10:37:22 +0530

    struture created

---
+++ src/main/java/com/nesa/hostpitaldemo/appoinment/domain/Appoinment.java
@@ -0,0 +1,13 @@
+package com.nesa.hostpitaldemo.appoinment.domain;
+
+import java.time.LocalDateTime;
+
+public class Appoinment {
+    private Long id;
+    private String number;
+    // private Doctor doctor;
+    // private Patient patient;
+    private LocalDateTime createdAt;
+    // private User createdBy;
+    private String status;
+}
